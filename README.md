f781c747a190fd135adf2109caedad88b03fd7b7
muHum <muHum@MDESKONE>
2020-05-04 06:35:45 +0200

    A little cleanup of config.class.example.php and a little update for the README.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# dyndns
-Small DynDNS Script (which works with Plesk Onyx)
+# dyndns - modded by muHum
+**Basic DynDNS Script for rustilldawn.net**
 
-German Install Guide: https://www.weyand.biz/2017/07/12/dyndns-mit-plesk-ipv4-ipv6-und-php7-224.html
-
-Deutsche Installations Anleitung: https://www.weyand.biz/2017/07/12/dyndns-mit-plesk-ipv4-ipv6-und-php7-224.html
+* Based on Tim Weylands [dyndns script](https://github.com/TimWeyand/dyndns)
+* With some inspiration thanks to a modded version of the script by [checkip.net](https://checkip.net/download/dyndns-mit-plesk-ipv4-ipv6-und-php7-mod/)
+* Original german installation guide: https://www.weyand.biz/2017/07/12/dyndns-mit-plesk-ipv4-ipv6-und-php7-224.html
--- private/config.class.example.php
+++ private/config.class.example.php
@@ -13,20 +13,24 @@ class config {
     const mysqlServer = 'localhost';
     const mysqlPort = '3306';
     const mysqlUsername = 'dyndns';
-    const mysqlPassword = 'yrEs9mGzvZSE7en9';
+    const mysqlPassword = 'UseSafePasswordHere';
     const mysqlDatabase = 'psa';
 
     //you can restrict the usage of this module - but you do not have to.
     const allowedDynDNSDomains = array();
 
-    const applicationHash = '2GrSup2xUWuQgAvwKLSzDAj3XcGuWeh3Asd';
+    const applicationHash = 'YouWantToChangeMe';
 
     // modified by muHum based on the version from checkip.net
     // possible values [false,'basic','db','rtdauth']
     const authentication = 'rtdauth';
 
     // rtdauth data
-    const myDynDNSDomain = 'YourDDNSDomainHere';
-    const ddns_auth_username = 'UseSafeUsername';
+    const myDynDNSDomain = 'my.ddns.server.abc';
+    const ddns_auth_username = 'UseSafeUsernameHere';
     const ddns_auth_password = 'UseSafePasswordHere';
+
+    // basic authentication data
+    const basic_auth_username = 'username';
+    const basic_auth_password = 'password';
 }
